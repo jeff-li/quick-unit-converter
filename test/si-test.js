d01dc94256444b86a278b1152285ce10ce23b24c
Jeff Li <jeff.weizhong.li@gmail.com>
2017-04-25 01:24:09 -0400

    convert simple SI units, more tests

--- test/si-test.js
+++ test/si-test.js
@@ -4,15 +4,16 @@ var expect = require('chai').expect;
 var getSIUnits = require('../lib/definitions/si-mapping.js')
 
 describe('SI Generator test', function() {
-    it('should return a an object', function() {
+    it('should return a a non-empty object', function() {
         var result = getSIUnits();
         expect(result).to.be.a('object');
+        expect(result).to.not.be.empty;
     });
 
-    // it('should return undefined if value is not a number', function() {
-    //     var result = converter('a', 'kg').to('g');
-    //     expect(result).to.equal(undefined);
-    // });
+    it('should include meter', function() {
+        var result = getSIUnits();
+        expect(result.m).to.deep.equal({multiplier: 1, type: 'length', name: '' });
+    });
 
 });
 
--- test/test.js
+++ test/test.js
@@ -6,12 +6,28 @@ var converter = require('../lib/converter');
 describe('#numFormatter', function() {
     it('should return a number', function() {
         var result = converter(1, 'kg').to('g');
-        expect(result).to.equal(123);
+        expect(result).to.be.a('number');
     });
 
     it('should return undefined if value is not a number', function() {
         var result = converter('a', 'kg').to('g');
         expect(result).to.equal(undefined);
+    });
+
+    it('should return undefined if params are missing', function() {
+        expect(converter('a').to('g')).to.equal(undefined);
+        expect(converter(12, 'm').to()).to.equal(undefined);
+        expect(converter().to('g')).to.equal(undefined);
+    });
+
+    it('should return convert km to m', function() {
+        var result = converter(100, 'km').to('m');
+        expect(result).to.equal(100000);
+    });
+
+    it('should return convert m to km', function() {
+        var result = converter(200, 'm').to('km');
+        expect(result).to.equal(0.2);
     });
 
 });
